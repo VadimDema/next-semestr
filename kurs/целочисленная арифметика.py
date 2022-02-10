4a1ad836a55d657f832eab96ba98283bba2c2cc9
VadimDema <90608445+VadimDema@users.noreply.github.com>
2022-02-10 11:54:37 +0300

    Update целочисленная арифметика.py

--- kurs/целочисленная арифметика.py
+++ kurs/целочисленная арифметика.py
@@ -1,1 +1,20 @@
+#шаг6 урок 2.5
+b = int(input())
+q = int(input())
+n = int(input())
+nn = (n - 1)
+print( b * (q ** nn))
 
+#шаг7 урок 2.5
+n=int(input())
+print(n // 100)
+
+#шаг8 урок 2.5
+h=int(input())
+m=int(input())
+print(m//h)
+print(m%h)
+
+#шаг9 урок 2.5
+n=int(input())
+print(n//2+n%2)
